37374b0424d37399a18b6a6cc79f1182d26dbfae
SowatRafi <sowat.hossian.rafi@g.bracu.ac.bd>
2020-12-02 00:23:40 +0600

    Created a Python Script. For downloading and give a name for that video.

---
+++ Single Video.py
@@ -0,0 +1,12 @@
+import pytube
+url = input("Enter Your YouTube link...\n")
+video = pytube.YouTube(url)
+stream = video.streams.get_by_itag(22)
+print("DOWNLOADING YOUR VIDEO>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
+stream.download(filename=f"{input('Enter your file name... ')}")
+print("DONE>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
+
+'''for stream in video.streams:
+    if "video" in str(stream) and "mp4" in str(stream):
+        print(stream)
+'''
